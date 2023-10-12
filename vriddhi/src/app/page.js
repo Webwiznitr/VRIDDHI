0413310a2a28e95a0d86c510a54fda0639ae8ab8
Anirudha <72496314+4n1rudh4@users.noreply.github.com>
2023-10-13 02:18:36 +0530

    Update page.js

--- vriddhi/src/app/page.js
+++ vriddhi/src/app/page.js
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Score from "./Components/scorecard/page";
-import Contact from "./Components/contact/page";
+import Contact from "./Components/contact/page"
 
 export default function Home() {
   return (
