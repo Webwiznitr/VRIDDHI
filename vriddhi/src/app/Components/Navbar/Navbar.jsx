c78ee7009bc8bb7b01294bb59d032ce542efb95c
Swadhin Kumar Nayak <nayak.swadhin25@gmail.com>
2023-10-13 12:07:19 +0530

    landing page mobile view

--- vriddhi/src/app/Components/Navbar/Navbar.jsx
+++ vriddhi/src/app/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import Image from "next/image";
 import vriddhiLogo from "public/Images/vriddhiLogo.svg";
 import NITRLogo from "public/Images/NITRLogo.svg";
+import Hamburger from "hamburger-react";
 const navComponents = [
   { title: "TOURNAMENT", id: 1 },
   { title: "GALLERY", id: 2 },
@@ -13,43 +15,61 @@ const navComponents = [
 ];
 
 function Navbar() {
+  const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="flex justify-around items-center z-10 relative">
-      <a href="/">
-        <Image
-          src={vriddhiLogo}
-          alt="vriddhi"
-          height={"300"}
-          width={"220"}
-          className="cursor-pointer"
-        />
-      </a>
-      <div className="flex items-center">
-        <div>
-          {navComponents.map((navComponent) => (
-            <Link
-              href={"/"}
-              key={navComponent.id}
-              className="text-white text-sm font-normal py-6 px-5 inline-flex font-nidus hover:text-blue-800"
-            >
-              {navComponent.title}
-            </Link>
-          ))}
+    <>
+      <nav className="md:flex justify-between items-center z-20 fixed w-full h-auto top-0">
+        <div className="flex justify-between items-center">
+          <a href="/">
+            <Image
+              src={vriddhiLogo}
+              alt="vriddhi"
+              height={"200"}
+              width={"200"}
+              className="cursor-pointer px-2"
+            />
+          </a>
+          <span className="md:hidden relative px-4">
+            <Hamburger
+              toggled={navbar}
+              toggle={() => setNavbar((navbar) => !navbar)}
+              duration={0.5}
+            />
+          </span>
         </div>
-        <button className="font-nidus text-[#D1713C] text-base px-5 border-[1.257px] border-solid border-[#D1713C] rounded-[18.858px] py-2 hover:text-[#f19665] hover:border-[#f19665]">
-          REGISTER
-        </button>
-      </div>
-      <a href="https://www.nitrkl.ac.in/" target="blank">
-        <Image
-          src={NITRLogo}
-          alt="NITR LOGO"
-          height={"87"}
-          width={"75"}
-          className="py-2 cursor-pointer"
-        />
-      </a>
-    </nav>
+        <ul
+          className={`${navbar ? "block" : "hidden"} md:flex md:items-center`}
+        >
+          {navComponents.map((navComponent) => (
+            <li key={navComponent.id}>
+              <Link
+                onClick={() => setNavbar((navbar) => !navbar)}
+                href={"/"}
+                className="text-white text-xs font-normal py-6 px-5 font-nidus hover:text-blue-800"
+              >
+                {navComponent.title}
+              </Link>
+            </li>
+          ))}
+          <button
+            className={`font-nidus text-[#D1713C] text-sm md:text-base mx-2 md:mx-0 px-3 md:px-5 border-[1.257px] border-solid border-[#D1713C] rounded-[18.858px] py-1 my-1 mb-2 md:my-0 md:py-2 hover:text-[#f19665] hover:border-[#f19665] ${
+              navbar ? "block" : "hidden"
+            } md:flex`}
+          >
+            REGISTER
+          </button>
+        </ul>
+        <a href="https://www.nitrkl.ac.in/" target="blank">
+          <Image
+            src={NITRLogo}
+            alt="NITR LOGO"
+            height={"87"}
+            width={"75"}
+            className="py-2 cursor-pointer hidden md:block"
+          />
+        </a>
+      </nav>
+    </>
   );
 }
 
