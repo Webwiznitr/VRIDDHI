40a234bd89835eaade424c55fd68fa4df95aa206
Anirudha <72496314+4n1rudh4@users.noreply.github.com>
2023-10-14 16:09:56 +0530

    Add files via upload

--- vriddhi/src/app/page.js
+++ vriddhi/src/app/page.js
@@ -5,12 +5,13 @@ import React from 'react';
 import Image from "next/image";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
-import Score from "./Components/scorecard/page";
+import Score from "./Components/scorecard/score";
 import Contact from "./Components/Contact/page"
 import Tournaments from '../tournaments/tournaments'
-import About from './components/About'
+import About from "./components/About"
 import Team from './components/Team'
 import Gallery from './Components/Gallery/gallery';
+
 export default function Home() {
   return (
     <>
