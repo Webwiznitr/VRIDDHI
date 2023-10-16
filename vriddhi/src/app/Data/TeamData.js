ed869a43ebe37a81b8401f08a68c357162d39090
Anirudha <72496314+4n1rudh4@users.noreply.github.com>
2023-10-16 09:42:59 +0530

    Update TeamData.js

--- vriddhi/src/app/Data/TeamData.js
+++ vriddhi/src/app/Data/TeamData.js
@@ -52,59 +52,5 @@ const TeamData = [
                   
 
 ]
-=======
-const TeamData = [
-
-    {
-     id:1,
-     name: "John Doe",
-     image: "/images/Team/rectangle.png",
-     info: "member",
-
-    },
-
-    {
-        id:2,
-        name: "John Doe",
-        image: "/images/Team/rectangle.png",
-        info: "member",
-   
-       },
-   
-    {
-        id:3,
-        name: "John Doe",
-        image: "/images/Team/rectangle.png",
-        info: "member",
-   
-    },
-   
-    {
-        id:4,
-        name: "John Doe",
-        image: "/images/Team/rectangle.png",
-        info: "member",
-   
-    },
-   
-    {
-        id:5,
-        name: "John Doe",
-        image: "/images/Team/rectangle.png",
-        info: "member",
-   
-    },
-   
-    {
-        id:6,
-        name: "John Doe",
-        image: "/images/Team/rectangle.png",
-        info: "member",
-   
-    },
-   
-                  
-
-]
 
 export default TeamData;
