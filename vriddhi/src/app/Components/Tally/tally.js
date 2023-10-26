848c3a9b3750184d94917acbcb785de535af846a
SpryzenAd <adhishsahu001@gmail.com>
2023-10-26 12:04:37 +0530

    updated css and added logo

--- vriddhi/src/app/Components/Tally/tally.js
+++ vriddhi/src/app/Components/Tally/tally.js
@@ -6,8 +6,8 @@ import silvermedal from './assets/silvermedal.png';
 
 const Tally = () => {
     const teams = [
-        { rank: 1, logo:'', name: 'NIT RKL', gold: 4, silver: 1 },
-        { rank: 2, logo:'', name: 'NIT SURATHKAL', gold: 3, silver: 1 },
+        { rank: 1, logo:'/images/logos/nitrkl.png', name: 'NIT RKL', gold: 4, silver: 1 },
+        { rank: 2, logo:'/images/logos/nitsur.png', name: 'NIT SURATHKAL', gold: 3, silver: 1 },
         { rank: 3, logo:'', name: 'NIT TRICHY', gold: 2, silver: 1 },
         { rank: 4, logo:'', name: 'BIT', gold: 1, silver: 1 },
         { rank: 5, logo:'', name: 'BIT', gold: 1, silver: 1 },
@@ -17,16 +17,16 @@ const Tally = () => {
       ];
     
       return (
-        <div className="min-w-full">
-        <div className="text-center">MEDAL TABLE</div>
-        <table className="w-3/4 m-auto rounded-[15px] bg-gradient-to-br from-[rgb(0,31,78)] to [rgb(0,25,63)]">
+        <div className="min-w-full bg-[rgb(24,24,33)]">
+        <h1 className="text-center sm:text-4xl md:text-5xl lg:text-6xl text-3xl py-8">MEDAL TABLE</h1>
+        <table className="w-3/4 m-auto font-sans sm:text-sm lg:text-base text-xs rounded-[15px] uppercase bg-gradient-to-br from-[rgb(0,31,78)] to-[rgb(0,5,13)]">
           <thead>
             <tr>
-              <th className="px-3 py-3 text-centre">
+              <th className="pl-3 pr-1 py-3 text-centre lg:text-lg font-normal">
                 Rank
               </th>
-              <th className="px-6 py-3 text-centre">
-                Team Name
+              <th className="pl-4 pr-5 py-3 text-centre lg:text-lg font-normal">
+                Name of the team
               </th>
               <th className="px-2 py-3">
                 <Image src={goldmedal} width={35} height={35} />
@@ -43,8 +43,11 @@ const Tally = () => {
                     (team.rank === 2 ? "text-gray-400" :
                     (team.rank === 3 ? "text-amber-700" : "text-white")
                 )}>
-                <td className="px-3 py-4 text-center uppercase">{team.rank}</td>
-                <td className="px-6 py-4 text-white">{team.name}</td>
+                <td className="pl-3 pr-1 py-4 text-center">{team.rank}</td>
+                <td className="pl-4 pr-5 py-4 text-white flex">
+                  <Image src={team.logo} width={25} height={25} className="mr-2" />
+                  {team.name}
+                </td>
                 <td className="px-4 py-4">{team.gold}</td>
                 <td className="px-4 py-4">{team.silver}</td>
               </tr>
