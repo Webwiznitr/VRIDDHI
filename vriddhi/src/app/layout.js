ddd666c2024075151f15d0c531da4f870f54941f
Amlan Sarangi <117941624+amlansarangi23@users.noreply.github.com>
2023-11-01 00:24:22 +0530

    Update layout.js

--- vriddhi/src/app/layout.js
+++ vriddhi/src/app/layout.js
@@ -1,33 +1,35 @@
-
+"use client";
+import React, { useState } from "react";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./Components/Navbar/Navbar";
-import connectMongoDB from '../../libs/mongodb'
+import connectMongoDB from "../../libs/mongodb";
 import Footer from "./Components/Footer/page";
 import Head from "next/head";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Vriddhi 2023",
   description: "Vriddhi 2023",
-
 };
 
 export default function RootLayout({ children }) {
-  
+  const [loading, setLoading] = useState(true);
+  setTimeout(() => {
+    setLoading(false);
+  }, 3000);
   return (
     <html lang="en">
-    <Head>
-        <link rel='icon' href='../favicon.ico'/>
+      <Head>
+        <link rel="icon" href="../favicon.ico" />
       </Head>
-      <body className={inter.className}>
-        <Navbar />
-        {children}
-      <Footer/>
-      </body>
 
+      {loading ? <></> : <Navbar />}
+
+      <body className={inter.className}>{children}</body>
+
+      {loading ? <></> : <Footer />}
     </html>
   );
 }
