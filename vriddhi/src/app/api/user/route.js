dde1c371b154aba5b5d7bdb92d9826a2f3a56596
4n1rudh4 <anirudha.talukdar@gmail.com>
2023-11-02 02:34:01 +0530

    a

--- vriddhi/src/app/api/user/route.js
+++ vriddhi/src/app/api/user/route.js
@@ -2,9 +2,9 @@ import { NextResponse} from "next/server";
 import connectMongoDB from "../../../../libs/mongodb";
 import  user  from "../../../../models/user";
 export async function POST(request){
-    const{email,name,contact,Wnumber,events,elist,participants,coaches,url,transactionid}= await request.json();
+const{email,name,ins,Wnumber,events,elist,participants,coaches/*,url,transactionid*/}= await request.json();
     await connectMongoDB();
-    await user.create({email,name,contact,Wnumber,events,elist,participants,coaches,url,transactionid});
+    await user.create({email,name,ins,Wnumber,events,elist,participants,coaches/*,url,transactionid*/});
     return NextResponse.json({message:"User registered"},{status:201})
 
 }
