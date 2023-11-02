5324065dae87f8309cf8c1f8f087a80967fd7f8c
Anirudha <72496314+4n1rudh4@users.noreply.github.com>
2023-11-02 11:50:38 +0530

    Update route.js

--- vriddhi/src/app/api/user/route.js
+++ vriddhi/src/app/api/user/route.js
@@ -2,9 +2,9 @@ import { NextResponse} from "next/server";
 import connectMongoDB from "../../../../libs/mongodb";
 import  user  from "../../../../models/user";
 export async function POST(request){
-const{email,name,ins,Wnumber,events,elist,participants,coaches/*,url,transactionid*/}= await request.json();
+const{email,name,ins,Wnumber,events,elist,participants,coaches,logo/*,url,transactionid*/}= await request.json();
     await connectMongoDB();
-    await user.create({email,name,ins,Wnumber,events,elist,participants,coaches/*,url,transactionid*/});
+    await user.create({email,name,ins,Wnumber,events,elist,participants,coaches,logo/*,url,transactionid*/});
     return NextResponse.json({message:"User registered"},{status:201})
 
 }
